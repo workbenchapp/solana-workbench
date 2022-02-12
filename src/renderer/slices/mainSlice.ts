07fd174eaa723c2928b672584354b070b89bdaa4
Nathan LeClaire <nathan.leclaire@gmail.com>
2022-02-11 19:42:42 -0800

    Add redux pieces
    Signed-off-by: Nathan LeClaire <nathan.leclaire@gmail.com>

---
+++ src/renderer/slices/mainSlice.ts
@@ -0,0 +1,34 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { ValidatorState } from 'types/types';
+
+const validatorState: ValidatorState = {
+  running: false,
+  waitingForRun: false,
+  loading: false,
+};
+
+export const mainSlice = createSlice({
+  name: 'main',
+  initialState: {
+    validatorState,
+  },
+  reducers: {
+    setValidatorState: (state, action: PayloadAction<ValidatorState>) => {
+      state.validatorState = action.payload;
+    },
+    setValidatorWaitingForRun: (state, action: PayloadAction<boolean>) => {
+      state.validatorState.waitingForRun = action.payload;
+    },
+    setValidatorStateLoading: (state, action: PayloadAction<boolean>) => {
+      state.validatorState.loading = action.payload;
+    },
+  },
+});
+
+export type RootState = ReturnType<typeof mainSlice.reducer>;
+export const {
+  setValidatorState,
+  setValidatorWaitingForRun,
+  setValidatorStateLoading,
+} = mainSlice.actions;
+export default mainSlice.reducer;
