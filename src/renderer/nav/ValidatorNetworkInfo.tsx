95012e06d98f3a9166105d4f0c534a8e0f192955
Sven Dowideit <SvenDowideit@home.org.au>
2022-03-08 15:40:53 +1000

    Add a page that shows the network's version infos

---
+++ src/renderer/nav/ValidatorNetworkInfo.tsx
@@ -0,0 +1,80 @@
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from 'renderer/slices/mainSlice';
+import { ValidatorNetworkInfoResponse } from '../../types/types';
+
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css';
+import BootstrapTable from 'react-bootstrap-table-next';
+import paginationFactory from 'react-bootstrap-table2-paginator';
+
+const ValidatorNetworkInfo = () => {
+  const validator = useSelector((state: RootState) => state.validator);
+
+  const columns = [
+    {
+      text: 'Pub Key',
+      dataField: 'pubkey',
+    },
+    {
+      text: 'Version',
+      dataField: 'version',
+    },
+  ];
+
+  const [data, setData] = useState<ValidatorNetworkInfoResponse>({
+    version: "unknown",
+    nodes: []
+  });
+
+  useEffect(() => {
+    const { net } = validator;
+    window.electron.ipcRenderer.fetchValidatorNetworkInfo({
+      net: net,
+    });
+  }, [validator]);
+
+  useEffect(() => {
+    const listener = (resp: any) => {
+      const { method, res } = resp;
+
+      switch (method) {
+        case 'get-validator-network-info':
+          console.log('ValidatorNetworkInfo', { res });
+          if (res) {
+            setData(res);
+          }
+          break;
+        default:
+      }
+    };
+    window.electron.ipcRenderer.on('main', listener);
+    return () => {
+      window.electron.ipcRenderer.removeListener('main', listener);
+    };
+  }, []);
+
+
+  // TODO: maybe show te version spread as a histogram and feature info ala
+  // solana --url mainnet-beta feature status
+  return (
+    <div className="col">
+      <div className="row">
+        <span className='column'>Current Network: {validator.net}</span>
+        <span className='column'>Current Version: {data.version}</span>
+      </div>
+      <div className="row">
+        <BootstrapTable
+          keyField='pubkey'
+          data={data.nodes}
+          columns={columns}
+          pagination={paginationFactory({
+            paginationSize: 25,
+          })}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ValidatorNetworkInfo;
