b7a571bd3303588860963f768f742713eefada6b
Sven Dowideit <SvenDowideit@home.org.au>
2022-03-14 08:52:21 +1000

    Correct Docker path on Linux and bump Solana version (#40)
    Signed-off-by: Sven Dowideit <SvenDowideit@home.org.au>

--- src/main/validator.ts
+++ src/main/validator.ts
@@ -12,9 +12,9 @@ import { logger } from './logger';
 const DOCKER_IMAGE =
   process.arch === 'arm64'
     ? 'nathanleclaire/solana:v1.9.2'
-    : 'solanalabs/solana:v1.9.2';
+    : 'solanalabs/solana:v1.9.11';
 let DOCKER_PATH = 'docker';
-if (process.platform !== 'win32') {
+if (process.platform === 'darwin') {
   DOCKER_PATH = '/usr/local/bin/docker';
 }
 
