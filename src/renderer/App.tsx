515542610c730902f69182638505d6e030532725
Nathan LeClaire <nathan.leclaire@gmail.com>
2022-03-16 16:23:41 -0700

    Update .erb dir
    Signed-off-by: Nathan LeClaire <nathan.leclaire@gmail.com>

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -21,14 +21,15 @@ import {
 import { useEffect, useState } from 'react';
 import useInterval from 'common/hooks';
 import { useSelector, useDispatch } from 'react-redux';
+import analytics from 'common/analytics';
+import { Button, Form } from 'react-bootstrap';
+
 import { configActions, RootState, validatorActions } from './slices/mainSlice';
 import { ConfigAction, ConfigKey, Net, NetStatus } from '../types/types';
-import analytics from 'common/analytics';
 import Toast from './components/Toast';
 import Accounts from './nav/Accounts';
 import Anchor from './nav/Anchor';
 import Validator from './nav/Validator';
-import { Button, Form } from 'react-bootstrap';
 
 declare global {
   interface Window {
@@ -129,7 +130,7 @@ export default function App() {
   useEffect(() => {
     const listener = (resp: any) => {
       const { method, res } = resp;
-      if (method != 'program-changes') {
+      if (method !== 'program-changes') {
         console.log(resp);
       }
       switch (method) {
@@ -179,7 +180,6 @@ export default function App() {
       </span>
     );
   }
-
 
   const netDropdownTitle = (
     <>
