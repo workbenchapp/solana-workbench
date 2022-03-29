a7e1fafe8b1316ed16bf95dcbf65d73cb4229d22
Nathan LeClaire <nathan.leclaire@gmail.com>
2022-03-29 16:47:20 -0400

    Add POC for validator runner

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/cryptoworkbench/solana-workbench/v2
+
+go 1.17
+
+require github.com/mitchellh/go-homedir v1.1.0
---
+++ workval/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path"
+	"strings"
+
+	"github.com/mitchellh/go-homedir"
+)
+
+func main() {
+	home, err := homedir.Dir()
+	if err != nil {
+		panic(err)
+	}
+	workbenchDir := path.Join(home, ".solana-workbench")
+	valArgs := []string{"--reset", "--ledger", path.Join(workbenchDir, "test-ledger")}
+	valCmd := exec.Command("solana-test-validator", valArgs...)
+	accountFiles, err := ioutil.ReadDir(path.Join(workbenchDir, "db", "accounts"))
+	for _, accountFile := range accountFiles {
+		valArgs = append(valArgs, "--account", accountFile.Name())
+	}
+    if err != nil {
+        panic(err)
+    }
+	bpfPrograms, err := ioutil.ReadDir(path.Join(workbenchDir, "db", "programs"))
+	for _, bpfProgram := range bpfPrograms {
+		valArgs = append(valArgs, "--bpf-program", bpfProgram.Name())
+	}
+    if err != nil {
+        panic(err)
+    }
+	fmt.Println("solana-test-validator", strings.Join(valArgs, " "))
+	fmt.Println("")
+	valCmd.Stdout = os.Stdout
+	valCmd.Stderr = os.Stderr
+	valCmd.Run()
+}
