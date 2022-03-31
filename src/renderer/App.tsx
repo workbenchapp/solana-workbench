105b7acac9d8c0b98f58c9bf4f9b83f35291a86d
Sven Dowideit <SvenDowideit@home.org.au>
2022-03-31 10:24:36 +1000

    reapply lit fixes
    Signed-off-by: Sven Dowideit <SvenDowideit@home.org.au>

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -2,9 +2,9 @@ import {
   MemoryRouter as Router,
   Switch,
   Route,
-  NavLink
+  NavLink,
 } from 'react-router-dom';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
 import './App.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -14,9 +14,9 @@ import Dropdown from 'react-bootstrap/Dropdown';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
-import Tooltip from 'react-bootstrap/Tooltip'
-import { SizeProp } from '@fortawesome/fontawesome-svg-core'
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
+import { SizeProp } from '@fortawesome/fontawesome-svg-core';
 
 import {
   faBook,
@@ -56,18 +56,18 @@ const NetworkSelector = () => {
     if (eventKey) dispatch(validatorActions.setNet(eventKey as Net));
   };
 
-  let statusText = validator.status as string
-  let statusClass = 'text-danger'
+  let statusText = validator.status as string;
+  let statusClass = 'text-danger';
   if (validator.status === NetStatus.Running) {
-      statusText = 'Available'
-      statusClass = 'sol-green'
+    statusText = 'Available';
+    statusClass = 'sol-green';
   }
   const statusDisplay = (
-      <span className="badge p-2">
-          <FontAwesomeIcon className={statusClass} icon={faCircle} />
-          {statusText}
-      </span>
-  )
+    <span className="badge p-2">
+      <FontAwesomeIcon className={statusClass} icon={faCircle} />
+      {statusText}
+    </span>
+  );
 
   const netDropdownTitle = (
     <>
@@ -79,28 +79,28 @@ const NetworkSelector = () => {
 
   return (
     <DropdownButton
-    size="sm"
-    id="dropdown-basic-button"
-    title={netDropdownTitle}
-    onSelect={netDropdownSelect}
-    className="ms-2 float-end"
-    align="end"
-  >
-    <Dropdown.Item eventKey={Net.Localhost} href="#">
-      {Net.Localhost}
-    </Dropdown.Item>
-    <Dropdown.Item eventKey={Net.Dev} href="#">
-      {Net.Dev}
-    </Dropdown.Item>
-    <Dropdown.Item eventKey={Net.Test} href="#">
-      {Net.Test}
-    </Dropdown.Item>
-    <Dropdown.Item eventKey={Net.MainnetBeta} href="#">
-      {Net.MainnetBeta}
-    </Dropdown.Item>
-  </DropdownButton>
+      size="sm"
+      id="dropdown-basic-button"
+      title={netDropdownTitle}
+      onSelect={netDropdownSelect}
+      className="ms-2 float-end"
+      align="end"
+    >
+      <Dropdown.Item eventKey={Net.Localhost} href="#">
+        {Net.Localhost}
+      </Dropdown.Item>
+      <Dropdown.Item eventKey={Net.Dev} href="#">
+        {Net.Dev}
+      </Dropdown.Item>
+      <Dropdown.Item eventKey={Net.Test} href="#">
+        {Net.Test}
+      </Dropdown.Item>
+      <Dropdown.Item eventKey={Net.MainnetBeta} href="#">
+        {Net.MainnetBeta}
+      </Dropdown.Item>
+    </DropdownButton>
   );
-}
+};
 
 function TooltipNavItem({
   to = '/',
@@ -111,16 +111,16 @@ function TooltipNavItem({
   iconsize = '1x',
 }) {
   return (
-      <OverlayTrigger
-          key={`${eventKey}-right`}
-          placement="right"
-          overlay={<Tooltip id="tooltip-right">{tooltipMessage}</Tooltip>}
-      >
-          <NavLink /*eventKey={eventKey}*/ to={to} className="nav-link">
-              <FontAwesomeIcon size={iconsize as SizeProp} icon={icon} /> {title}
-          </NavLink>
-      </OverlayTrigger>
-  )
+    <OverlayTrigger
+      key={`${eventKey}-right`}
+      placement="right"
+      overlay={<Tooltip id="tooltip-right">{tooltipMessage}</Tooltip>}
+    >
+      <NavLink /* eventKey={eventKey} */ to={to} className="nav-link">
+        <FontAwesomeIcon size={iconsize as SizeProp} icon={icon} /> {title}
+      </NavLink>
+    </OverlayTrigger>
+  );
 } // TODO: work out how propTypes work with fontAwesome
 TooltipNavItem.propTypes = {
   to: PropTypes.string.isRequired,
@@ -129,63 +129,63 @@ TooltipNavItem.propTypes = {
   eventKey: PropTypes.string.isRequired,
   icon: PropTypes.element.isRequired,
   iconsize: PropTypes.string.isRequired,
-}
+};
 // TODO: work out TooltipNavItem.defaults
 
 function Sidebar() {
   return (
-      <Navbar className="l-navbar" bg="light" expand="sm">
-          <nav className="nav">
-              <TooltipNavItem
-                  to="/"
-                  title=""
-                  tooltipMessage="Changes"
-                  eventKey="changes"
-                  icon={faTh}
-                  iconsize="2x"
-              />
-              <TooltipNavItem
-                  to="/validator"
-                  title=""
-                  tooltipMessage="Validator"
-                  eventKey="validator"
-                  icon={faBook}
-                  iconsize="2x"
-              />
-              <TooltipNavItem
-                  to="/anchor"
-                  title=""
-                  tooltipMessage="Anchor"
-                  eventKey="anchor"
-                  icon={faAnchor}
-                  iconsize="2x"
-              />
-              <TooltipNavItem
-                  to="/validatornetworkinfo"
-                  title=""
-                  tooltipMessage="Network Info"
-                  eventKey="validatornetworkinfo"
-                  icon={faNetworkWired}
-                  iconsize="2x"
-              />
-          </nav>
-      </Navbar>
-  )
+    <Navbar className="l-navbar" bg="light" expand="sm">
+      <nav className="nav">
+        <TooltipNavItem
+          to="/"
+          title=""
+          tooltipMessage="Changes"
+          eventKey="changes"
+          icon={faTh}
+          iconsize="2x"
+        />
+        <TooltipNavItem
+          to="/validator"
+          title=""
+          tooltipMessage="Validator"
+          eventKey="validator"
+          icon={faBook}
+          iconsize="2x"
+        />
+        <TooltipNavItem
+          to="/anchor"
+          title=""
+          tooltipMessage="Anchor"
+          eventKey="anchor"
+          icon={faAnchor}
+          iconsize="2x"
+        />
+        <TooltipNavItem
+          to="/validatornetworkinfo"
+          title=""
+          tooltipMessage="Network Info"
+          eventKey="validatornetworkinfo"
+          icon={faNetworkWired}
+          iconsize="2x"
+        />
+      </nav>
+    </Navbar>
+  );
 }
 
 function Topbar() {
   return (
-      <Navbar sticky="top" bg="primary" variant="dark" expand="sm">
-          <Container fluid>
-              <Navbar.Brand href="#">Solana Workbench</Navbar.Brand>
-              <Navbar.Toggle aria-controls="navbarScroll" />
-              <Navbar.Collapse id="navbarScroll">
-                  <Nav
-                      className="me-auto my-2 my-lg-0"
-                      style={{ maxHeight: '100px' }}
-                      navbarScroll
-                  >
-                      {/* <TooltipNavItem
+    <Navbar sticky="top" bg="primary" variant="dark" expand="sm">
+      <Container fluid>
+        <Navbar.Brand href="#">Solana Workbench</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            {/* <TooltipNavItem
                           to="/"
                           title="Changes"
                           tooltipMessage="Changes"
@@ -217,15 +217,69 @@ function Topbar() {
                           icon={faNetworkWired}
                           iconsize="xl"
                       /> */}
-                  </Nav>
-                  <Form className="d-flex">
-                      <NetworkSelector />
-                  </Form>
-              </Navbar.Collapse>
-          </Container>
-      </Navbar>
-  )
+          </Nav>
+          <Form className="d-flex">
+            <NetworkSelector />
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
+
+const AnalyticsBanner = () => {
+  const [analyticsEnabled, setAnalyticsEnabled] = useState(true);
+
+  return (
+    <div className="container">
+      <div className="mt-2">
+        <h3>Will you help us out?</h3>
+        Workbench collects telemetry data to improve your experience. You can
+        audit this code on{' '}
+        <a
+          href="https://github.com/workbenchapp/solana-workbench"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>
+        . You can opt out below.
+      </div>
+      <div className="mt-2 mb-2">
+        <h5>What We Collect</h5>
+        <ul>
+          <li>Which features are popular</li>
+          <li>System properties like OS version</li>
+          <li>How often people are using Workbench</li>
+        </ul>
+        We do not collect addresses or private keys.
+      </div>
+      <Form>
+        <Form.Check
+          type="switch"
+          className="d-inline-block"
+          id="analytics-ok-switch"
+          label="Yes, enable telemetry"
+          checked={analyticsEnabled}
+          onClick={() => setAnalyticsEnabled(!analyticsEnabled)}
+        />
+        <Button
+          className="ms-2"
+          variant="primary"
+          onClick={() => {
+            window.electron.ipcRenderer.config({
+              action: ConfigAction.Set,
+              key: ConfigKey.AnalyticsEnabled,
+              val: analyticsEnabled ? 'true' : 'false',
+            });
+          }}
+        >
+          <span className="ms-1 text-white">OK</span>
+        </Button>
+      </Form>
+    </div>
+  );
+};
 
 export default function App() {
   const dispatch = useDispatch();
@@ -284,35 +338,33 @@ export default function App() {
     keyExists: !(`${ConfigKey.AnalyticsEnabled}` in config.values),
   });
   if (!(`${ConfigKey.AnalyticsEnabled}` in config.values)) {
-    mainDisplay = (
-      <AnalyticsBanner />
-    );
+    mainDisplay = <AnalyticsBanner />;
   } else {
     console.log('Rendering alternative page...', { config });
     mainDisplay = (
       <div className="vh-100">
-          <Topbar />
-          <Sidebar />
-          {toasts.map((t) => (
-            <Toast {...t} />
-          ))}
-        <Container >
-        <Row className="mt-3">
-          <Route exact path="/">
-            <Accounts />
-          </Route>
-          <Route path="/validator">
-            <Validator />
-          </Route>
-          <Route path="/anchor">
-            <Anchor />
-          </Route>
-          <Route path="/validatornetworkinfo">
-            <ValidatorNetworkInfo />
-          </Route>
-        </Row>
+        <Topbar />
+        <Sidebar />
+        {toasts.map((t) => (
+          <Toast {...t} />
+        ))}
+        <Container>
+          <Row className="mt-3">
+            <Route exact path="/">
+              <Accounts />
+            </Route>
+            <Route path="/validator">
+              <Validator />
+            </Route>
+            <Route path="/anchor">
+              <Anchor />
+            </Route>
+            <Route path="/validatornetworkinfo">
+              <ValidatorNetworkInfo />
+            </Route>
+          </Row>
         </Container>
-        </div>
+      </div>
     );
   }
 
@@ -320,59 +372,5 @@ export default function App() {
     <Router>
       <Switch>{mainDisplay}</Switch>
     </Router>
-  );
-}
-
-const AnalyticsBanner = () => {
-  const [analyticsEnabled, setAnalyticsEnabled] = useState(true);
-
-  return (
-    <div className="container">
-    <div className="mt-2">
-      <h3>Will you help us out?</h3>
-      Workbench collects telemetry data to improve your experience. You can
-      audit this code on{' '}
-      <a
-        href="https://github.com/workbenchapp/solana-workbench"
-        target="_blank"
-        rel="noreferrer"
-      >
-        Github
-      </a>
-      . You can opt out below.
-    </div>
-    <div className="mt-2 mb-2">
-      <h5>What We Collect</h5>
-      <ul>
-        <li>Which features are popular</li>
-        <li>System properties like OS version</li>
-        <li>How often people are using Workbench</li>
-      </ul>
-      We do not collect addresses or private keys.
-    </div>
-    <Form>
-      <Form.Check
-        type="switch"
-        className="d-inline-block"
-        id="analytics-ok-switch"
-        label="Yes, enable telemetry"
-        checked={analyticsEnabled}
-        onClick={() => setAnalyticsEnabled(!analyticsEnabled)}
-      />
-      <Button
-        className="ms-2"
-        variant="primary"
-        onClick={() => {
-          window.electron.ipcRenderer.config({
-            action: ConfigAction.Set,
-            key: ConfigKey.AnalyticsEnabled,
-            val: analyticsEnabled ? 'true' : 'false',
-          });
-        }}
-      >
-        <span className="ms-1 text-white">OK</span>
-      </Button>
-    </Form>
-  </div>
   );
 }
