0987a0cb39189045d489fb6166ad83e6300ad1ef
Nathan LeClaire <nathan.leclaire@gmail.com>
2022-04-19 16:45:35 -0700

    remove wack import
    Signed-off-by: Nathan LeClaire <nathan.leclaire@gmail.com>

--- src/renderer/data/Config/configState.ts
+++ src/renderer/data/Config/configState.ts
@@ -1,5 +1,4 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { stat } from 'fs';
 
 // https://redux.js.org/usage/usage-with-typescript#define-slice-state-and-action-types
 // eslint-disable-next-line import/no-cycle
