f98a9d9013a2abd89b5958d4e7d9a9cdcc6127e0
Nathan LeClaire <nathan@cryptoworkbench.io>
2022-05-05 21:56:59 -0500

    Bump Solana to v1.9.20 (#106)
    * Bump Solana to v1.9.20
    
    Signed-off-by: Nathan LeClaire <nathan.leclaire@gmail.com>
    
    * Patch up issues w/ new version
    
    Signed-off-by: Nathan LeClaire <nathan.leclaire@gmail.com>
    
    * Fix up inspect
    
    Signed-off-by: Nathan LeClaire <nathan.leclaire@gmail.com>

--- src/main/validator.ts
+++ src/main/validator.ts
@@ -5,8 +5,8 @@ import { logger } from './logger';
 
 const DOCKER_IMAGE =
   process.arch === 'arm64'
-    ? 'cryptoworkbench/solana'
-    : 'solanalabs/solana:v1.9.11';
+    ? 'cryptoworkbench/solana:v1.9.20'
+    : 'solanalabs/solana:v1.9.20';
 let DOCKER_PATH = 'docker';
 if (process.platform === 'darwin') {
   DOCKER_PATH = '/usr/local/bin/docker';
@@ -19,9 +19,23 @@ const runValidator = async () => {
   }
 
   // TODO: and now test if there's a Docker daemon up
-
   try {
-    await execAsync(`${DOCKER_PATH} inspect solana-test-validator`);
+    const { stdout } = await execAsync(
+      `${DOCKER_PATH} inspect solana-test-validator`
+    );
+    const inspectOutput = JSON.parse(stdout)[0];
+    const running = inspectOutput.State.Running;
+    const exitCode = inspectOutput.State.ExitCode;
+    if (!running && exitCode !== 0) {
+      // eslint-disable-next-line no-console
+      console.log(
+        await execAsync(`${DOCKER_PATH} logs --tail 100 solana-test-validator`)
+      );
+      logger.error('Removing solana-test-validator...');
+      // eslint-disable-next-line no-console
+      console.log(await execAsync(`${DOCKER_PATH} rm solana-test-validator`));
+      throw new Error("Container exists, but isn't running.");
+    }
   } catch (e) {
     logger.error(e);
     // TODO: check for image, pull if not present
@@ -35,20 +49,17 @@ const runValidator = async () => {
         -p 8900:8900/tcp \
         -p 9900:9900/tcp \
         -p 10000:10000/tcp \
-        -p 10000-10011:10000-10011/udp \
+        -p 10000-10020:10000-10020/udp \
         --log-driver local \
         --ulimit nofile=1000000 \
         ${DOCKER_IMAGE} \
         solana-test-validator \
-        --dynamic-port-range 10000-10011 \
+        --dynamic-port-range 10000-10020 \
         --ledger test-ledger \
         --no-bpf-jit \
         --log`
     );
-
-    return;
   }
-  await execAsync(`${DOCKER_PATH} start solana-test-validator`);
 };
 
 const validatorLogs = async (msg: ValidatorLogsRequest) => {
