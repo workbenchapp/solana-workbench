380a669ed26fafd2f0ca8a3c33327184df4ad00f
Sven Dowideit <SvenDowideit@home.org.au>
2022-07-15 13:19:44 +1000

    don't crash because we're not connected
    Signed-off-by: Sven Dowideit <SvenDowideit@home.org.au>

--- src/renderer/nav/TokenPage.tsx
+++ src/renderer/nav/TokenPage.tsx
@@ -77,6 +77,27 @@ function TokenPage() {
   }
   const myWallet = publicKey;
 
+  if (status !== NetStatus.Running) {
+    return (
+      <div className="h-full w-full justify-center items-center flex flex-col">
+        <div className="relative z-0 flex flex-col items-center">
+          <svg
+            viewBox="0 0 200 200"
+            className="absolute transform -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2 w-80 h-80 -z-1"
+          >
+            <path
+              fill="rgb(var(--surface-300))"
+              d="M41.7,-52.4C52.9,-40.3,60.2,-26.1,67,-8.7C73.8,8.7,80.2,29.4,72.9,42.1C65.6,54.9,44.5,59.8,25.6,63.7C6.7,67.7,-10.1,70.7,-25.1,66.3C-40.1,62,-53.3,50.2,-61.3,35.7C-69.4,21.2,-72.4,3.9,-70.2,-13.1C-68.1,-30.1,-60.8,-47,-48.3,-58.9C-35.7,-70.9,-17.9,-77.9,-1.3,-76.3C15.2,-74.8,30.5,-64.6,41.7,-52.4Z"
+              transform="translate(100 100)"
+            />
+          </svg>
+          <IconMdiWarning className="text-6xl z-1" />
+          <span className="z-2">Network Not Available</span>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <Stack className="almost-vh-100">
       <Row>
