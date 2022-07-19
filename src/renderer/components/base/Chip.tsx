2d22d5afd037c8edb5781192f488ae9a9ba67bdf
Nathan LeClaire <nathan.leclaire@gmail.com>
2022-07-19 11:39:30 -0400

    Remove vite-inline-css plugin

--- src/renderer/components/base/Chip.tsx
+++ src/renderer/components/base/Chip.tsx
@@ -1,15 +1,3 @@
-import { css } from 'vite-plugin-inline-css-modules';
-
-const classes = css`
-  .chip {
-    @apply bg-surface-100 border-2 border-surface-300/50 w-min p-1 px-2 rounded-full select-none cursor-pointer bg-opacity-10 transition duration-100 hover:bg-surface-300 whitespace-nowrap;
-
-    &.active {
-      @apply bg-primary-base;
-    }
-  }
-`;
-
 const Chip: React.FC<
   React.DetailedHTMLProps<
     React.ButtonHTMLAttributes<HTMLButtonElement>,
@@ -20,7 +8,7 @@ const Chip: React.FC<
 > = ({ children, active, ...rest }) => {
   return (
     <button
-      className={`${classes.chip} ${active ? classes.active : undefined}`}
+      className={`chip ${active && 'chip-active'}`}
       type="button"
       {...rest}
     >
